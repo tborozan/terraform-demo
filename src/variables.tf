b61170d13618641d7b8a42ab258c6a5df7254508
Toni Borozan <tborozan@hotmail.com>
2022-04-24 12:56:48 +0200

    set vars as sensitive

--- src/variables.tf
+++ src/variables.tf
@@ -7,19 +7,27 @@ variable "AZUREREGION" {
 variable "SUBSCRIPTIONID" {
   type        = string
   description = "The Azure subscription ID"
+  sensitive   = true
+  default     = "$"
 }
 
 variable "TENANTID" {
   type        = string
   description = "The Azure tenant ID"
+  sensitive   = true
+  default     = "$"
 }
 
 variable "CLIENTID" {
   type        = string
   description = "The Azure client ID"
+  sensitive   = true
+  default     = "$"
 }
 
 variable "CLIENTSECRET" {
   type        = string
   description = "The Azure client secret"
+  sensitive   = true
+  default     = "$"
 }
