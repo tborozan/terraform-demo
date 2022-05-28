31638d074bbda00aa965333a89d40bf14e5b1921
Toni Borozan <tborozan@hotmail.com>
2022-05-28 09:30:19 +0200

    removed default values

--- src/variables.tf
+++ src/variables.tf
@@ -8,26 +8,22 @@ variable "SUBSCRIPTIONID" {
   type        = string
   description = "The Azure subscription ID"
   sensitive   = true
-  default     = "$"
 }
 
 variable "TENANTID" {
   type        = string
   description = "The Azure tenant ID"
   sensitive   = true
-  default     = "$"
 }
 
 variable "CLIENTID" {
   type        = string
   description = "The Azure client ID"
   sensitive   = true
-  default     = "$"
 }
 
 variable "CLIENTSECRET" {
   type        = string
   description = "The Azure client secret"
   sensitive   = true
-  default     = "$"
 }
